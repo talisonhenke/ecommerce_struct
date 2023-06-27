3a022fc3f48496b0baeed394a9d6a04c12600187
talisonhenke <talison.henke@gmail.com>
2023-06-27 16:06:26 -0300

    .env change app_url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
   <link href="assets/css/bootstrap.min.css" rel="stylesheet">
   <script src="assets/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" type="text/css" href="assets/css/app.css">
-  <link rel="stylesheet" type="text/css" href="bootstrap-icons-1.10.5/font/bootstrap-icons.css">
+  {{-- <link rel="stylesheet" type="text/css" href="bootstrap-icons-1.10.5/font/bootstrap-icons.css"> --}}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <style type="text/css">
     @media only screen and (min-width: 767px){
       .dropdown:hover .dropdown-menu{
